b27e7fda4ffba0ed708b5b0ce84e3e0abe8f5170
Zero0Q <elvissalihovic.es@gmail.com>
2025-01-20 18:15:15 +0800

    Update addon.js

--- addon.js
+++ addon.js
@@ -20,45 +20,43 @@ export default {
     async getMetas(type = MOVIE, providers = ['nfx'], country = "GB", language = 'en') {
         let res = null;
         try {
-            res = await axios.post('https://apis.justwatch.com/graphql', {
-                "operationName": "GetPopularTitles",
-                "variables": {
-                    "popularTitlesSortBy": "NEWEST", // Changed from "NEW" to "NEWEST"
-                    "first": AMOUNT,
-                    "platform": "WEB",
-                    "sortRandomSeed": 0,
-                    "popularAfterCursor": "",
-                    "offset": null,
-                    "popularTitlesFilter": {
-                        "ageCertifications": [],
-                        "excludeGenres": [],
-                        "excludeProductionCountries": [],
-                        "genres": [],
-                        "objectTypes": [
-                            type
-                        ],
-                        "productionCountries": [],
-                        "packages": providers,
-                        "excludeIrrelevantTitles": false,
-                        "presentationTypes": [],
-                        "monetizationTypes": [
-                            "FREE",
-                            "FLATRATE",
-                            "ADS"
-                        ]
-                    },
-                    "language": language,
-                    "country": country
-                },
-                "query": "query GetPopularTitles(\n  $country: Country!\n  $popularTitlesFilter: TitleFilter\n  $popularAfterCursor: String\n  $popularTitlesSortBy: PopularTitlesSorting! = NEWEST\n  [...]"
-            });
-        } catch (e) {
-            console.error("ERROR MESSAGE-------------------------", e.message);
-            console.log("ERROR RESPONSE-------------------------", e.response?.data);
+    res = await axios.post('https://apis.justwatch.com/graphql', {
+        "operationName": "GetNewestTitles", // Updated query name
+        "variables": {
+            "popularTitlesSortBy": "NEWEST", // Ensure this is set correctly
+            "first": AMOUNT,
+            "platform": "WEB",
+            "sortRandomSeed": 0,
+            "popularAfterCursor": "",
+            "offset": null,
+            "popularTitlesFilter": {
+                "ageCertifications": [],
+                "excludeGenres": [],
+                "excludeProductionCountries": [],
+                "genres": [],
+                "objectTypes": [
+                    type
+                ],
+                "productionCountries": [],
+                "packages": providers,
+                "excludeIrrelevantTitles": false,
+                "presentationTypes": [],
+                "monetizationTypes": [
+                    "FREE",
+                    "FLATRATE",
+                    "ADS"
+                ]
+            },
+            "language": language,
+            "country": country
+        },
+        "query": "query GetNewestTitles(\\n  $country: Country!\\n  $popularTitlesFilter: TitleFilter\\n  $popularAfterCursor: String\\n  $popularTitlesSortBy: PopularTitlesSorting! = NEWEST\\n  $first: Int!\\n  $language: Language!\\n  $offset: Int = 0\\n  $sortRandomSeed: Int! = 0\\n  $profile: PosterProfile\\n  $backdropProfile: BackdropProfile\\n  $format: ImageFormat\\n) {\\n  popularTitles(\\n    country: $country\\n    filter: $popularTitlesFilter\\n    offset: $offset\\n    after: $popularAfterCursor\\n    sortBy: $popularTitlesSortBy\\n    first: $first\\n    sortRandomSeed: $sortRandomSeed\\n  ) {\\n    totalCount\\n    pageInfo {\\n      startCursor\\n      endCursor\\n      hasPreviousPage\\n      hasNextPage\\n      __typename\\n    }\\n    edges {\\n      ...PopularTitleGraphql\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment PopularTitleGraphql on PopularTitlesEdge {\\n  cursor\\n  node {\\n    id\\n    objectId\\n    objectType\\n    content(country: $country, language: $language) {\\n      externalIds {\\n        imdbId\\n      }\\n      title\\n      fullPath\\n      scoring {\\n        imdbScore\\n        __typename\\n      }\\n      posterUrl(profile: $profile, format: $format)\\n      ... on ShowContent {\\n        backdrops(profile: $backdropProfile, format: $format) {\\n          backdropUrl\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n  __typename\\n}\"\n    });
+} catch (e) {
+    console.error("ERROR MESSAGE-------------------------", e.message);
+    console.log("ERROR RESPONSE-------------------------", e.response?.data);
 
-            return [];
-        }
-
+    return [];
+}
         console.log(providers.join(','), res.data.data.popularTitles.edges.length);
         let metasResults = [];
         const results = await Promise.allSettled(res.data.data.popularTitles.edges.map(async (item, index) => {
