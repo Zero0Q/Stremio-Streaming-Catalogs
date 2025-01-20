140211e968e4be95f5a2bb3c1ed15ac29fc2f218
Zero0Q <elvissalihovic.es@gmail.com>
2025-01-20 17:49:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.set('trust proxy', true)
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'vue', 'dist')));
 
+
 let mixpanel = null;
 if (process.env.MIXPANEL_KEY) {
     mixpanel = Mixpanel.init(process.env.MIXPANEL_KEY);
@@ -37,43 +38,25 @@ let movies = providers.filter(provider => provider.regions?.movie).reduce((acc, 
 let series = providers.filter(provider => provider.regions?.show).reduce((acc, provider)=>{
     acc[provider.id] = [];
     return acc;
-}, {});
-
+}, {})
 async function loadNewCatalog() {
     console.log('loadNewCatalog');
 
-    let mergedCatalog = await mergeCatalogs();
-
-    if (state.sortByLatest) {
-        mergedCatalog = sortByLatest(mergedCatalog);
-    }
-
-    // Further processing with mergedCatalog
-    console.log('done');
-}
-
-// Function to merge catalogs
-async function mergeCatalogs() {
-    let mergedCatalog = [];
+    const movies = {};
+    const series = {};
 
     await Promise.all(providers.map(async provider => {
         if (provider.regions.movie) {
-            const movieCatalog = await addon.getMetas(MOVIE, [provider.id], provider.regions.movie, provider.language);
-            mergedCatalog = mergedCatalog.concat(movieCatalog);
+            movies[provider.id] = await addon.getMetas(MOVIE, [provider.id], provider.regions.movie, provider.language);
         }
         if (provider.regions.show) {
-            const showCatalog = await addon.getMetas(SHOW, [provider.id], provider.regions.show, provider.language);
-            mergedCatalog = mergedCatalog.concat(showCatalog);
+            series[provider.id] = await addon.getMetas(SHOW, [provider.id], provider.regions.show, provider.language);
         }
     }));
 
-    return mergedCatalog;
+    console.log('done');
 }
 
-// Function to sort catalogs by latest releases
-function sortByLatest(catalog) {
-    return catalog.sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate));
-}
 
 app.get('/:configuration/manifest.json', (req, res) => {
     res.setHeader('Cache-Control', 'max-age=86400,stale-while-revalidate=86400,stale-if-error=86400,public');
@@ -94,61 +77,42 @@ app.get('/:configuration/manifest.json', (req, res) => {
 
     let catalogs = [];
 
-    if (state.mergeCatalogs) {
-        loadNewCatalog().then(mergedCatalog => {
-            catalogs = mergedCatalog;
-            res.send({
-                id: APP_ID,
-                logo: APP_LOGO,
-                version: APP_VERSION,
-                name: 'All Streaming Catalogs',
-                description: 'Your favourite streaming services!',
-                catalogs: catalogs,
-                resources: ['catalog'],
-                types: [MOVIE.toLowerCase(), SERIES.toLowerCase()],
-                idPrefixes: ['tt'],
-                behaviorHints: {
-                    configurable: true,
-                }
-            });
-        });
-    } else {
-        selectedProviders.split(',').forEach(providerId => {
-            const provider = providers.find(p => p.id === providerId);
-            if (provider) {
-                if (provider.regions?.movie) {
-                    catalogs.push({
-                        id: providerId,
-                        type: MOVIE.toLowerCase(),
-                        name: provider.name,
-                    });
-                }
-                if (provider.regions?.show) {
-                    catalogs.push({
-                        id: providerId,
-                        type: SERIES.toLowerCase(),
-                        name: provider.name,
-                    });
-                }
+    selectedProviders.split(',').forEach(providerId => {
+        const provider = providers.find(p => p.id === providerId);
+        if (provider) {
+            if (provider.regions?.movie) {
+                catalogs.push({
+                    id: providerId,
+                    type: MOVIE.toLowerCase(),
+                    name: provider.name,
+                });
             }
-        });
-
-        res.send({
-            id: APP_ID,
-            logo: APP_LOGO,
-            version: APP_VERSION,
-            name: 'All Streaming Catalogs',
-            description: 'Your favourite streaming services!',
-            catalogs: catalogs,
-            resources: ['catalog'],
-            types: [MOVIE.toLowerCase(), SERIES.toLowerCase()],
-            idPrefixes: ['tt'],
-            behaviorHints: {
-                configurable: true,
+            if (provider.regions?.show) {
+                catalogs.push({
+                    id: providerId,
+                    type: SERIES.toLowerCase(),
+                    name: provider.name,
+                });
             }
-        });
-    }
-});
+        }
+    });
+    
+    // show catalogs for providers
+    res.send({
+        id: APP_ID,
+        logo: APP_LOGO,
+        version: APP_VERSION,
+        name: 'All Streaming Catalogs',
+        description: 'Your favourite streaming services!',
+        catalogs: catalogs,
+        resources: ['catalog'],
+        types: [MOVIE.toLowerCase(), SERIES.toLowerCase()],
+        idPrefixes: ['tt'],
+        behaviorHints: {
+            configurable: true,
+        }
+    });
+})
 
 app.get('/:configuration?/catalog/:type/:id/:extra?.json', (req, res) => {
     res.setHeader('Cache-Control', 'max-age=86400,stale-while-revalidate=86400,stale-if-error=86400,public');
@@ -157,6 +121,8 @@ app.get('/:configuration?/catalog/:type/:id/:extra?.json', (req, res) => {
     // parse config
     const buffer = Buffer(req.params?.configuration || '', 'base64');
     let [selectedProviders, rpdbKey, countryCode, installedAt] = buffer.toString('ascii')?.split(':');
+
+    //console.log(selectedProviders, rpdbKey, countryCode, installedAt);
 
     if (String(rpdbKey || '').startsWith('16')) {
         installedAt = rpdbKey;
@@ -167,7 +133,7 @@ app.get('/:configuration?/catalog/:type/:id/:extra?.json', (req, res) => {
         ip: req.ip,
         distinct_id: req.ip.replace(/\.|:/g, 'Z'),
         configuration: req.params?.configuration,
-        selectedProviders: selectedProviders.split(','),
+        selectedProviders:selectedProviders.split(','),
         rpdbKey,
         countryCode,
         installedAt,
@@ -175,6 +141,8 @@ app.get('/:configuration?/catalog/:type/:id/:extra?.json', (req, res) => {
         catalog_id: req.params.id,
         catalog_extra: req.params?.extra,
     });
+
+   
 
     let id = req.params.id;
     // legacy addon, netflix-only catalog support
@@ -188,14 +156,16 @@ app.get('/:configuration?/catalog/:type/:id/:extra?.json', (req, res) => {
 
     if (req.params.type === 'movie') {
         res.send({ metas: addon.replaceRpdbPosters(rpdbKey, movies[id]) });
+
         return;
     }
 
     if (req.params.type === 'series') {
         res.send({ metas: addon.replaceRpdbPosters(rpdbKey, series[id]) });
+
         return;
     }
-});
+})
 
 app.get('/manifest.json', function (req, res) {
     res.setHeader('Cache-Control', 'max-age=86400,stale-while-revalidate=86400,stale-if-error=86400,public');
@@ -206,23 +176,23 @@ app.get('/manifest.json', function (req, res) {
         distinct_id: req.ip.replace(/\.|:/g, 'Z'),
     });
 
-    let defaultCatalogs = [];
+    let defaultCatelogs=[];
     providers.filter(provider => provider.default).forEach(provider => {
-        if (provider.regions.movie) {
-            defaultCatalogs.push({
+        if(provider.regions.movie) {
+            defaultCatelogs.push({
                 id: provider.id,
                 type: MOVIE.toLowerCase(),
                 name: provider.name,
-            });
+            })
         }
-        if (provider.regions.show) {
-            defaultCatalogs.push({
+        if(provider.regions.show) {
+            defaultCatelogs.push({
                 id: provider.id,
                 type: SERIES.toLowerCase(),
                 name: provider.name,
-            });
+            })
         }
-    });
+    })
 
     res.send({
         id: APP_ID,
@@ -230,7 +200,7 @@ app.get('/manifest.json', function (req, res) {
         version: APP_VERSION,
         name: 'All Streaming Catalogs',
         description: 'Trending movies and series on Netflix, HBO Max, Disney+, Apple TV+ and more. Configure to choose your favourite services.',
-        catalogs: defaultCatalogs,
+        catalogs: defaultCatelogs,
         resources: ['catalog'],
         types: [MOVIE.toLowerCase(), SERIES.toLowerCase()],
         idPrefixes: ['tt'],
@@ -238,7 +208,7 @@ app.get('/manifest.json', function (req, res) {
             configurable: true,
         }
     });
-});
+})
 
 // fallback to Vue
 app.get(/.*/, (req, res) => {
